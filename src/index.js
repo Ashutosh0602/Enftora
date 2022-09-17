163c5a9a58ca030da591b6c67db6c82d1bfcac02
Ashutosh0602 <ashu00jnp@gmail.com>
2022-09-17 10:15:15 +0530

    fourth commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { Provider } from "react-redux";
 import App from "./App";
+import dataStore from "./components/SepRedux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={dataStore}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
