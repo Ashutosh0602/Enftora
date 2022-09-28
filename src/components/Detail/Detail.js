d827177c05b2e00e2e552206248611e10594ed58
Ashutosh0602 <ashu00jnp@gmail.com>
2022-09-29 00:45:29 +0530

    tenth commit

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./detail.module.css";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -14,48 +14,55 @@ export default function Detail() {
       setfullnft(dt[0].assets);
     });
   });
+
   const detailNft = fullnft.filter((ls) => ls.id == token);
-  console.log(detailNft[0]);
-  return (
-    <section className={classes.detail_sec}>
-      <div>{detailNft[0]["name"]}</div>
-      <div>{detailNft[0]["id"]}</div>
-      <div>{detailNft[0]["description"]}</div>
-      <div>
-        <div>
-          <img src={detailNft[0]["image_url"]} />
+
+  //   console.log(detailNft.length);
+  if (detailNft.length >= 1) {
+    return (
+      <section className={classes.detail_sec}>
+        <div className={classes.det_add}>{detailNft[0]["name"]}</div>
+        <div className={classes.det_add}>{detailNft[0]["id"]}</div>
+        <div className={classes.det_add}>{detailNft[0]["description"]}</div>
+        <div className={classes.det_nft}>
+          <div className={classes.def_nft_img_cont}>
+            <img
+              className={classes.def_nft_img}
+              src={detailNft[0]["image_url"]}
+            />
+          </div>
+          <div>
+            <div className={classes.det_nft_inn}>
+              <div>Address:</div>
+              <div>{detailNft[0]["asset_contract"]["address"]}</div>
+            </div>
+            <div className={classes.det_nft_inn}>
+              <div> Token id:</div>
+              <div>{detailNft[0]["token_id"]}</div>
+            </div>
+            <div className={classes.det_nft_inn}>
+              <div> Schema name:</div>
+              <div>{detailNft[0]["asset_contract"]["schema_name"]}</div>
+            </div>
+            <div className={classes.det_nft_inn}>
+              <div>Symbol:</div>
+              <div>{detailNft[0]["asset_contract"]["symbol"]}</div>
+            </div>
+            <div className={classes.det_nft_inn}>
+              <div>Collection Name:</div>
+              <div>{detailNft[0]["collection"]["name"]}</div>
+            </div>
+            <div className={classes.det_nft_inn}>
+              <div>Collection Slug:</div>
+              <div>{detailNft[0]["collection"]["slug"]}</div>
+            </div>
+            <div className={classes.det_nft_inn}>
+              <div>Created Date:</div>
+              <div>{detailNft[0]["asset_contract"]["created_date"]}</div>
+            </div>
+          </div>
         </div>
-        <div>
-          <div>
-            <div>Address</div>
-            <div>{detailNft[0]["asset_contract"]["address"]}</div>
-          </div>
-          <div>
-            <div> Token id</div>
-            <div>{detailNft[0]["token_id"]}</div>
-          </div>
-          <div>
-            <div> Schema name</div>
-            <div>{detailNft[0]["asset_contract"]["schema_name"]}</div>
-          </div>
-          <div>
-            <div>Symbol</div>
-            <div>{detailNft[0]["asset_contract"]["symbol"]}</div>
-          </div>
-          <div>
-            <div>Collection Name</div>
-            <div>{detailNft[0]["collection"]["name"]}</div>
-          </div>
-          <div>
-            <div>Collection Slug</div>
-            <div>{detailNft[0]["collection"]["slug"]}</div>
-          </div>
-          <div>
-            <div>Address</div>
-            <div>{detailNft[0]["asset_contract"]["address"]}</div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
